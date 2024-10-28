92caa9ea4186744221511c55cb7861c54d0cd1af
Lucas Vinicius <lu.viniteixeira78@gmail.com>
2024-10-28 11:04:23 -0300

    Finishing CRUD

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const products = [
 ]
 
 function buscarProdutosPorId(id){
-    return products.filter( product => product.id == id)
+    return products.findIndex( product => product.id == id)
 }
 
 app.get('/', (req, res) => {
@@ -22,13 +22,26 @@ app.get('/products', (req, res) => {
     res.status(200).send(products)
 })
 
+app.get('/products/:id', (req, res) => {
+    res.send(products[buscarProdutosPorId(req.params.id)])
+})
+
 app.post('/products', (req, res) => {
     products.push(req.body)
     res.status(201).send("Product criated sucessfully")
 })
 
-app.delete(`/products/`, (req, res) => {
-    products.delete
+app.delete('/products/:id', (req, res) => {
+    products.splice(buscarProdutosPorId(req.params.id), 1)
+    res.send("Product deleted successfully")
+})
+
+app.put('/products/:id', (req, res) => {
+    let index = buscarProdutosPorId(req.params.id)
+    products[index].nome = req.body.nome
+    products[index].preco = req.body.preco
+    products[index].cor = req.body.cor
+    res.send("Product changed sucessfully")
 })
 
 export default app
